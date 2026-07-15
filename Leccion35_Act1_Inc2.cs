d5e7e11e24dcbaa5912d2ddd512bfd83b7056889
26KMU01-KevinMorataya <moratayakevin16db@gmail.com>
2026-07-15 09:05:30 -0600

    Add files via upload

---
+++ Leccion35_Act1_Inc2.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Kevin Morataya\n Grado: IVC \n Clave:24\n *****CALCULADORA DE CAMBIO****");
+        int pago;
+        string producto;
+        Console.WriteLine("Digite el nombre del producto: ");
+        producto = Console.ReadLine();
+        Console.Write("Dame el precio del producto");
+        int precio=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite el valor del billete");
+        pago=Convert.ToInt32(Console.ReadLine());
+        int cambio = pago - precio;
+        Console.WriteLine("Su cambio es: "+cambio);
+
+    }
+}
